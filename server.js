a7511793342925adb8c28191f8f3850e41bfd722
Om-Thanage <omsthanage2005@gmail.com>
2025-12-10 10:45:24 +0530

    feat: Add MongoDB connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dotenv.config();
+connectDB();
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
